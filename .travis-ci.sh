3ad376453ab20ce5be2f45376286a552a0ab98a3
gabrik <gabriele.baldoni@gmail.com>
2018-11-14 13:08:34 +0100

    update travis yml file to install dependencies
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,7 +7,6 @@ curl -L -o yaks.tar.gz https://www.dropbox.com/s/275sm1vux7ywvy3/yaks.tar.gz
 tar -xzvf yaks.tar.gz
 $YD/yaksd -w > $YD/yaks.out 2>&1 & echo $! > $YD/yaks.pid
 YPID=$(<"$YD/yaks.pid")
-cat $YD/yaks.out
 echo "YAKS PID $YPID"
 cd $WD
 echo "Running tests from $WD"

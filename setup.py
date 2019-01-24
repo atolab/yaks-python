3d69c3fdfc71bd0aa6e7ae1311e311b4acaf4f39
gabrik <gabriele.baldoni@gmail.com>
2019-01-24 12:36:55 +0100

    fix selector bug with = in value
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name='yaks',
-    version='0.2.1.post2',
+    version='0.2.1.post3',
     author='ADLINK Advance Technology Office',
     description='Python API to access the YAKS service',
     long_description=read('README.md'),

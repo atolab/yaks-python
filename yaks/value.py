2315442af5179a9e9b506e000f1cefa17138ea1d
kydos <angelo@icorsaro.net>
2019-02-22 11:29:51 +0100

    ensured that raw_format is always initialised

--- yaks/value.py
+++ yaks/value.py
@@ -30,10 +30,10 @@ class Value(object):
                 raise ValidationError("Value is not a valid JSON")
             self.value = json.dumps(value)
         elif self.encoding == Encoding.RAW and isinstance(value, str):
-            self.value = value.encode()
+            self.value = value.encode()            
         else:
             self.value = value
-            self.raw_format = raw_format
+        self.raw_format = raw_format
 
     def get_encoding(self):
         return self.encoding

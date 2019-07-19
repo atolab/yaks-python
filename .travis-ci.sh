8550765a10438acabaee40d46e5e02d92499e2b3
gabrik <gabriele.baldoni@gmail.com>
2019-07-19 15:37:04 +0200

    updated travis script
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,8 +6,10 @@ YD=$(pwd)
 # curl -L -o /usr/local/lib/libzenohc.so https://github.com/atolab/atobin/raw/master/zenoh-c/latest/ubuntu/16.04/libzenohc.so
 curl -L -o yaksd https://github.com/atolab/atobin/raw/master/yaks/latest/ubuntu/16.04/yaksd
 # tar -xzvf yaks.tar.gz
-$YD/yaksd -w --verbosity=debug > $YD/yaks.out 2>&1 & echo $! > $YD/yaks.pid
+chmod +x $YD/yaksd
+$YD/yaksd -w -z "zenohd" --verbosity=debug > $YD/yaks.out 2>&1 & echo $! > $YD/yaks.pid
 YPID=$(<"$YD/yaks.pid")
+sleep 2;
 cat $YD/yaks.out
 echo "YAKS PID $YPID"
 cd $WD

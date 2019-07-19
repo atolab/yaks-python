da87a3a58578e5801ccd2b21c0f339a1565133e1
gabrik <gabriele.baldoni@gmail.com>
2019-07-19 15:16:20 +0200

    Added download of libzenohc.so for test, updated yaks binary
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,8 +3,9 @@
 WD=$(pwd)
 cd $(mktemp -d)
 YD=$(pwd)
-curl -L -o yaks.tar.gz https://www.dropbox.com/s/g4tnzvjwlx3zcr2/yaksd.tar.gz
-tar -xzvf yaks.tar.gz
+curl -L -o /usr/local/lib/libzenohc.so https://github.com/atolab/atobin/raw/master/zenoh-c/latest/ubuntu/16.04/libzenohc.so
+curl -L -o yaksd https://github.com/atolab/atobin/raw/master/yaks/latest/ubuntu/16.04/yaksd
+# tar -xzvf yaks.tar.gz
 $YD/yaksd -w --verbosity=debug > $YD/yaks.out 2>&1 & echo $! > $YD/yaks.pid
 YPID=$(<"$YD/yaks.pid")
 cat $YD/yaks.out

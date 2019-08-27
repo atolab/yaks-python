8f29a60ee140b4823a3c727dbb47e4c300d39d47
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-08-27 17:50:39 +0200

    Fix travis build

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,20 +2,19 @@
 
 WD=$(pwd)
 cd $(mktemp -d)
-YD=$(pwd)
-# curl -L -o /usr/local/lib/libzenohc.so https://github.com/atolab/atobin/raw/master/zenoh-c/latest/ubuntu/16.04/libzenohc.so
-curl -L -o yaksd https://github.com/atolab/atobin/raw/master/yaks/latest/ubuntu/16.04/yaksd
-# tar -xzvf yaks.tar.gz
-chmod +x $YD/yaksd
-$YD/yaksd -w -z "zenohd" --verbosity=debug > $YD/yaks.out 2>&1 & echo $! > $YD/yaks.pid
-YPID=$(<"$YD/yaks.pid")
+RD=$(pwd)
+curl -L -o zenohd https://github.com/atolab/atobin/raw/master/zenoh/unstable/ubuntu/16.04/zenohd
+curl -L -o yaks-plugin.cmxs https://github.com/atolab/atobin/tree/master/yaks/unstable/ubuntu/16.04/yaks-plugin.cmxs
+chmod +x $RD/zenohd
+$RD/zenohd -P "yaks-plugin.cmxs -w" --verbosity=debug > $RD/zenohd.out 2>&1 & echo $! > $RD/zenohd.pid
+ZPID=$(<"$RD/zenohd.pid")
 sleep 2;
-cat $YD/yaks.out
-echo "YAKS PID $YPID"
+cat $YD/zenohd.out
+echo "ZENOHD PID $ZPID"
 cd $WD
 echo "Running tests from $WD"
 tox
-echo "KILLING YAKS $YPID"
-kill -15 $YPID
-rm -rf $YD
+echo "KILLING ZENOHD $ZPID"
+kill -15 $ZPID
+rm -rf $RD
 exit 0

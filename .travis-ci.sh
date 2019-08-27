1fc07183ea31c68725a37aeb9e609ba89c8a0508
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-08-27 17:53:02 +0200

    Fix travis build

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,12 +4,12 @@ WD=$(pwd)
 cd $(mktemp -d)
 RD=$(pwd)
 curl -L -o zenohd https://github.com/atolab/atobin/raw/master/zenoh/unstable/ubuntu/16.04/zenohd
-curl -L -o yaks-plugin.cmxs https://github.com/atolab/atobin/tree/master/yaks/unstable/ubuntu/16.04/yaks-plugin.cmxs
+curl -L -o yaks-plugin.cmxs https://github.com/atolab/atobin/raw/master/yaks/unstable/ubuntu/16.04/yaks-plugin.cmxs
 chmod +x $RD/zenohd
 $RD/zenohd -P "yaks-plugin.cmxs -w" --verbosity=debug > $RD/zenohd.out 2>&1 & echo $! > $RD/zenohd.pid
 ZPID=$(<"$RD/zenohd.pid")
 sleep 2;
-cat $YD/zenohd.out
+cat $RD/zenohd.out
 echo "ZENOHD PID $ZPID"
 cd $WD
 echo "Running tests from $WD"
